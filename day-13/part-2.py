5c9f5ca3425db169edc4a67e7ea05bb29cb3246c
JP Verkamp <me@jverkamp.com>
2015-12-13 17:22:54 -0800

    Day 13

---
+++ day-13/part-2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import collections
+import itertools
+import re
+import sys
+
+happiness = collections.defaultdict(lambda : collections.defaultdict(lambda : 0))
+for line in sys.stdin:
+    self, gain_lose, amount, other = re.match(
+        r'(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+).',
+        line
+    ).groups()
+
+    amount = int(amount)
+    if gain_lose == 'lose':
+        amount *= -1
+
+    happiness[self][other] = amount
+
+name = list(happiness.keys()) + ['me']
+
+best_score, best_ordering = max(
+    (
+        sum(
+            happiness[a][b] + happiness[b][a]
+            for a, b in zip(ordering, ordering[1:] + (ordering[0],))
+        ),
+        ordering
+    )
+    for ordering in itertools.permutations()
+)
+
+print(best_ordering)
+print(best_score)
