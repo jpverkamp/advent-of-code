aa69444fa571765865f785e3986271322b1ee889
JP Verkamp <me@jverkamp.com>
2017-12-23 14:56:13 -0500

    Tweak run all to be able to run ... not all

--- 2017/run-all.py
+++ 2017/run-all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
 import time
 import yaml
 
@@ -8,6 +9,9 @@ with open('test-cases.yaml') as fin:
 	data = yaml.load(fin)
 
 for folder in sorted(data):
+	if len(sys.argv) > 1 and folder not in sys.argv[1:]:
+		continue
+
 	for command in data[folder]:
 		start = time.time()
 		output = subprocess.check_output(command, shell = True, cwd = folder).decode().strip()
