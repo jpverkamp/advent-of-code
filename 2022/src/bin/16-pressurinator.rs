e4a358281d185ab47870b006a149671c01ef83c8
JP Verkamp <github@jverkamp.com>
2022-12-17 13:38:12 -0500

    Add a new multi agent mode (that doesn't match the test cases yet?)

--- 2022/src/bin/16-pressurinator.rs
+++ 2022/src/bin/16-pressurinator.rs
@@ -1,7 +1,7 @@
 use aoc::*;
-use im::{Vector};
+use im::{Vector, vector};
 use regex::Regex;
-use std::{cell::RefCell, hash::Hash, path::Path, rc::Rc, collections::HashMap};
+use std::{cell::RefCell, collections::HashMap, hash::Hash, path::Path, rc::Rc};
 
 #[derive(Debug)]
 struct Cave {
@@ -97,7 +97,6 @@ where
                 break;
             }
         }
-        
 
         Cave {
             size: names.len(),
@@ -111,6 +110,33 @@ where
 
 #[derive(Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
 struct Step(usize, String);
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
+struct State {
+    index: usize,
+    ttl: usize,
+}
+
+impl State {
+    fn new(index: usize) -> State {
+        State { index, ttl: 0 }
+    }
+
+    fn tick(self, ticks: usize) -> State {
+        State {
+            index: self.index,
+            ttl: self.ttl - ticks,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
+enum Step2 {
+    DoNothing{fuel: usize},
+    AdvanceTime{fuel: usize, ticks: usize},
+    EnableTick{fuel: usize, activations: Vec<(usize, String)>},
+    Schedule{fuel: usize, agent: usize, distance: usize, target: String},
+}
 
 impl Cave {
     fn max_flow(self, location: String, fuel: usize) -> (usize, Vector<Step>) {
@@ -145,7 +171,10 @@ impl Cave {
             // Don't bother moving to something that's already on
             // Don't bother moving to nodes with 0 flow
             for next_index in 0..cave.clone().size {
-                if index == next_index || enabled[next_index] || cave.clone().flow_rates[next_index] == 0 {
+                if index == next_index
+                    || enabled[next_index]
+                    || cave.clone().flow_rates[next_index] == 0
+                {
                     continue;
                 }
 
@@ -165,7 +194,9 @@ impl Cave {
                     next_enabled,
                 );
                 sub_result.0 += (d + 1) * per_tick_flow;
-                sub_result.1.push_front(Step(d, cave.clone().names[next_index].clone()));
+                sub_result
+                    .1
+                    .push_front(Step(d, cave.clone().names[next_index].clone()));
                 result = result.max(sub_result);
             }
 
@@ -182,12 +213,147 @@ impl Cave {
             Vector::from(vec![false; cave.clone().size]),
         )
     }
+
+    fn max_flow2(self, location: String, fuel: usize, agents: usize) -> (usize, Vector<Step2>) {
+        type CacheKey = (Vector<State>, usize, Vector<bool>);
+        type CacheValue = (usize, Vector<Step2>);
+
+        fn recur(
+            cave: Rc<Cave>,
+            cache: Rc<RefCell<HashMap<CacheKey, CacheValue>>>,
+            agents: Vector<State>,
+            fuel: usize,
+            enabled: Vector<bool>,
+        ) -> CacheValue {
+            let cache_key = (agents.clone(), fuel, enabled.clone());
+            if cache.borrow().contains_key(&cache_key) {
+                return cache.borrow_mut()[&cache_key].clone();
+            }
+
+            let per_tick_flow = cave
+                .clone()
+                .flow_rates
+                .iter()
+                .zip(enabled.clone().iter())
+                .filter_map(|(f, c)| if *c { Some(*f) } else { None })
+                .sum::<usize>();
+
+            // Base case: try doing nothing for the rest of the simulation
+            let mut result = (fuel * per_tick_flow, vector![Step2::DoNothing{fuel}]);
+
+            // If our TTL is 0, schedule our next move
+            // This doesn't advance time
+            if let Some((i, agent)) = agents.clone().iter().enumerate().find(|(_, a)| a.ttl == 0) {
+                for next_index in 0..cave.clone().size {
+                    if agents.clone().iter().any(|a| next_index == a.index)
+                        || enabled[next_index]
+                        || cave.clone().flow_rates[next_index] == 0
+                    {
+                        continue;
+                    }
+
+                    let d = cave.clone().distances[[agent.index, next_index]];
+                    if d + 1 > fuel {
+                        continue;
+                    }
+
+                    let mut new_agents = agents.clone();
+                    new_agents[i] = State{index: next_index, ttl: d + 1};
+
+                    let mut sub_result = recur(
+                        cave.clone(),
+                        cache.clone(),
+                        new_agents,
+                        fuel,
+                        enabled.clone(),
+                    );
+                    sub_result.1.push_front(Step2::Schedule{
+                        fuel,
+                        agent: i,
+                        distance: d,
+                        target: cave.clone().names[next_index].clone(),
+                    });
+                    result = result.max(sub_result);
+                }
+            }
+            // If either TTL is 1, enable that flow and tick 1
+            else if agents.clone().iter().any(|a| a.ttl == 1) {
+                let mut activations = Vec::new();
+
+                let mut next_enabled = enabled.clone();
+                for (i, agent) in agents.clone().iter().enumerate() {
+                    if agent.ttl == 1 {
+                        next_enabled[agent.index] = true;
+                        activations.push((i, cave.clone().names[agent.index].clone()));
+                    }
+                }
+
+                let mut next_agents = agents.clone();
+                for (i, agent) in agents.clone().iter().enumerate() {
+                    next_agents[i] = agent.tick(1);
+                }
+
+                let mut sub_result = recur(
+                    cave.clone(),
+                    cache.clone(),
+                    next_agents,
+                    fuel - 1,
+                    next_enabled,
+                );
+
+                sub_result.0 += per_tick_flow;
+                sub_result
+                    .1
+                    .push_front(Step2::EnableTick{
+                        fuel,
+                        activations,
+                    });
+
+                result = result.max(sub_result);
+            }
+            // Otherwise, advance by the the lower of the two
+            // Advance to ttl of 1 so that we catch the enable
+            else {
+                let ticks = agents.clone().iter().min_by(|a, b| a.ttl.cmp(&b.ttl)).expect("must have at least one agent").ttl - 1;
+
+                let mut next_agents = agents.clone();
+                for (i, agent) in agents.clone().iter().enumerate() {
+                    next_agents[i] = agent.tick(ticks);
+                }
+
+                let mut sub_result = recur(
+                    cave.clone(),
+                    cache.clone(),
+                    next_agents,
+                    fuel - ticks,
+                    enabled.clone(),
+                );
+
+                sub_result.0 += per_tick_flow * ticks;
+                sub_result.1.push_front(Step2::AdvanceTime { 
+                    fuel,
+                    ticks,
+                });
+                result = result.max(sub_result);
+            }
+
+            cache.borrow_mut().insert(cache_key, result.clone());
+            result
+        }
+
+        let cave = Rc::new(self);
+        recur(
+            cave.clone(),
+            Rc::new(RefCell::new(HashMap::new())),
+            Vector::from(vec![State::new(cave.clone().indexes[&location]); agents]),
+            fuel,
+            Vector::from(vec![false; cave.clone().size]),
+        )
+    }
 }
 
 fn part1(filename: &Path) -> String {
     let cave = Cave::from(&mut iter_lines(filename));
-
-    // println!("{:?}", cave);
 
     let (max_flow, steps) = cave.max_flow(String::from("AA"), 30);
     if true || cfg!(debug_assertions) {
@@ -200,7 +366,16 @@ fn part1(filename: &Path) -> String {
 }
 
 fn part2(filename: &Path) -> String {
-    todo!()
+    let cave = Cave::from(&mut iter_lines(filename));
+
+    let (max_flow, steps) = cave.max_flow2(String::from("AA"), 26, 2);
+    if true || cfg!(debug_assertions) {
+        for (_i, step) in steps.iter().enumerate() {
+            println!("{step:?}");
+        }
+    }
+
+    max_flow.to_string()
 }
 
 fn main() {
