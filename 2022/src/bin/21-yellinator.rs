66f272260dd8304995ea188a63ee9b773fc622c4
JP Verkamp <github@jverkamp.com>
2022-12-23 14:05:48 -0500

    Solved day 21

--- 2022/src/bin/21-yellinator.rs
+++ 2022/src/bin/21-yellinator.rs
@@ -1,10 +1,12 @@
 use aoc::*;
 use itertools::Itertools;
-use std::{collections::HashMap, path::Path, rc::Rc, cell::RefCell};
+use std::{collections::HashMap, path::Path, rc::Rc, cell::RefCell, fs::File, io::Write};
+
+type INumber = isize;
 
 #[derive(Debug)]
 struct Op {
-    f: fn(isize, isize) -> isize,
+    f: fn(INumber, INumber) -> INumber,
     name: String,
 }
 
@@ -17,7 +19,7 @@ impl From<&str> for Op {
                 "-" => |a, b| a - b, 
                 "*" => |a, b| a * b, 
                 "/" => |a, b| a / b, 
-                "=" => |a, b| if a == b { 1 } else { 0 }, 
+                "=" => |a, b| if a == b { 1 as INumber } else { 0 as INumber }, 
 
                 _ => panic!("unknown op: {text}"),
             },
@@ -25,24 +27,12 @@ impl From<&str> for Op {
     }
 }
 
-impl Op {
-    fn from_inverse(name: &str) -> Self {
-        match name {
-            "+" => Op::from("-"),
-            "-" => Op::from("+"),
-            "*" => Op::from("/"),
-            "/" => Op::from("*"),
-
-            _ => panic!("don't know the inverse of: {}", name)
-        }
-    }
-}
-
 #[allow(dead_code)]
 #[derive(Debug)]
 enum Monkey {
+    Human,
     Constant {
-        value: isize,
+        value: INumber,
     },
     Math {
         op: Op,
@@ -52,6 +42,13 @@ enum Monkey {
 }
 
 impl Monkey {
+    fn is_human(&self) -> bool {
+        match self {
+            Monkey::Human => true,
+            _ => false,
+        }
+    }
+
     fn try_op_name(&self) -> Option<String> {
         match self {
             Monkey::Math { op: Op { name, ..}, ..} => Some(name.clone()),
@@ -59,7 +56,7 @@ impl Monkey {
         }
     }
 
-    fn try_constant_value(&self) -> Option<isize> {
+    fn try_constant_value(&self) -> Option<INumber> {
         match self {
             Monkey::Constant { value } => Some(*value),
             _ => None,
@@ -108,7 +105,7 @@ impl Troop {
             );
 
             let value = value
-                .parse::<isize>()
+                .parse::<INumber>()
                 .expect("Constant monkey value must be numeric");
 
             self.monkeys
@@ -142,12 +139,13 @@ impl Troop {
         }
     }
 
-    fn value(&self, name: &String) -> isize {
+    fn value(&self, name: &String) -> INumber {
         match &self.monkeys[name].as_ref() {
             Monkey::Constant { value } => *value,
             Monkey::Math {
                 op: Op {f, ..}, left, right, ..
             } => f(self.value(left), self.value(right)),
+            _ => panic!("humans have no value"),
         }
     }
 
@@ -160,10 +158,10 @@ impl Troop {
                 !self.monkeys.iter().any(|(_, monkey)| {
                     potential.as_str() == "root"
                         || match monkey.as_ref() {
-                            Monkey::Constant { .. } => false,
                             Monkey::Math { left, right, .. } => {
                                 left == *potential || right == *potential
-                            }
+                            },
+                            _ => false,
                         }
                 })
             })
@@ -184,7 +182,6 @@ impl Troop {
 
             'found_one: for (name, monkey) in self.monkeys.iter() {
                 match monkey.as_ref() {
-                    Monkey::Constant { .. } => continue,
                     Monkey::Math {
                         op: Op {f, ..}, left, right, ..
                     } => {
@@ -208,7 +205,8 @@ impl Troop {
                             target = Some(response);
                             break 'found_one;
                         }
-                    }
+                    },
+                    _ => continue,
                 }
             }
 
@@ -223,70 +221,67 @@ impl Troop {
         self.remove_dead_nodes();
     }
 
-    // Simplify 
+    // Simplify cases where one first level and one second level child are constant
     fn simplify_equality(&mut self) {
         let rc_self = Rc::new(RefCell::new(self));
+        let mut name_count = 0;
 
-        let root_name = String::from("root");
-        let humn_name = String::from("humn");
-
-        'found_human: loop { 
-            let root = rc_self.clone().borrow().monkeys[&root_name].clone();
+        'found_human: for _i in 1.. { 
+            let root = rc_self.clone().borrow().monkeys[&String::from("root")].clone();
             if root.try_op_name().is_none() || root.try_op_name().unwrap() != "=" {
                 panic!("root must be = to use this method");
             }
 
-            let left = rc_self.clone().borrow().monkeys[&root.try_math_left().unwrap()].clone();
-            let right = rc_self.clone().borrow().monkeys[&root.try_math_right().unwrap()].clone();
+            let left_name = root.try_math_left().unwrap().clone();
+            let left = rc_self.clone().borrow().monkeys[&left_name].clone();
+
+            let right_name = root.try_math_right().unwrap().clone();
+            let right = rc_self.clone().borrow().monkeys[&right_name].clone();
+            
+            #[allow(unused_assignments)]
+            let mut v_level_1 = None; // First level value
+            #[allow(unused_assignments)]
+            let mut v_level_1_is_left = false;
+
+            #[allow(unused_assignments)]
+            let mut v_level_2 = None; // Second level value
+            #[allow(unused_assignments)]
+            let mut v_level_2_is_left = false;
+
+            #[allow(unused_assignments)]
+            let mut op_name = None;
+            #[allow(unused_assignments)]
+            let mut t_level_2 = None;
+
+            let mut to_remove = vec![
+                left_name.clone(),
+                right_name.clone(),
+            ];
+
+            if left.is_human() || right.is_human() {
+                break 'found_human;
+            }
 
             // Left is the constant side
             if let Some(lv) = left.try_constant_value() {
+                v_level_1 = Some(lv);
+                v_level_1_is_left = true;
+                op_name = Some(right.try_op_name().unwrap().clone());
+
                 // Right left is the other constant
                 if let Some(rlv) = rc_self.clone().borrow().monkeys[&right.try_math_left().unwrap()].try_constant_value() {
-                    if right.try_math_left().unwrap() == humn_name {
-                        break 'found_human;
-                    }
-
-                    // lv = R
-                    // lv = Rop(rlv, RR)
-                    // Rop'(rlv, lv) = RR
-                    let op_name = left.try_op_name().unwrap();
-                    let new_monkey_name = format!("{}{:?}{}", rlv, op_name, lv);
-                    let new_monkey = Monkey::Constant { value: (Op::from_inverse(op_name.as_str()).f)(rlv, lv) };
-                    rc_self.clone().borrow_mut().monkeys.insert(new_monkey_name.clone(), Rc::new(new_monkey));
-
-                    rc_self.clone().borrow_mut().monkeys.insert(
-                        root_name,
-                        Rc::new(Monkey::Math { 
-                            op: Op::from("="),
-                            left: new_monkey_name,
-                            right: right.try_math_right().unwrap(),
-                        }),
-                    );
+                    v_level_2 = Some(rlv);
+                    v_level_2_is_left = true;
+                    t_level_2 = Some(right.try_math_right().unwrap());
+                    to_remove.push(right.try_math_left().unwrap().clone());
                 }
 
                 // Right right is the other constant
                 else if let Some(rrv) = rc_self.clone().borrow().monkeys[&right.try_math_right().unwrap()].try_constant_value() {
-                    if right.try_math_right().unwrap() == humn_name {
-                        break 'found_human;
-                    }
-
-                    // lv = R
-                    // lv = Rop(RL, rrv)
-                    // Rop'(lv, rrv) = RR
-                    let op_name = left.try_op_name().unwrap();
-                    let new_monkey_name = format!("{}{:?}{}", lv, op_name, rrv);
-                    let new_monkey = Monkey::Constant { value: (Op::from_inverse(op_name.as_str()).f)(lv, rrv) };
-                    rc_self.clone().borrow_mut().monkeys.insert(new_monkey_name.clone(), Rc::new(new_monkey));
-
-                    rc_self.clone().borrow_mut().monkeys.insert(
-                        root_name,
-                        Rc::new(Monkey::Math { 
-                            op: Op::from("="),
-                            left: new_monkey_name,
-                            right: right.try_math_right().unwrap(),
-                        }),
-                    );
+                    v_level_2 = Some(rrv);
+                    v_level_2_is_left = false;
+                    t_level_2 = Some(right.try_math_left().unwrap());
+                    to_remove.push(right.try_math_right().unwrap().clone());
                 }
 
                 // Something went wrong
@@ -297,53 +292,24 @@ impl Troop {
 
             // Right is the constant side
             else if let Some(rv) = right.try_constant_value() {
+                v_level_1 = Some(rv);
+                v_level_1_is_left = false;
+                op_name = Some(left.try_op_name().unwrap().clone());
+
                 // Left left is the other constant
                 if let Some(llv) = rc_self.clone().borrow().monkeys[&left.try_math_left().unwrap()].try_constant_value() {
-                    if left.try_math_left().unwrap() == humn_name {
-                        break 'found_human;
-                    }
-
-                    // L = rv
-                    // Lop(llv, LR) = rv
-                    // LR = Lop'(llv, rv)
-                    let op_name = left.try_op_name().unwrap();
-                    let new_monkey_name = format!("{}{:?}{}", llv, op_name, rv);
-                    let new_monkey = Monkey::Constant { value: (Op::from_inverse(op_name.as_str()).f)(llv, rv) };
-                    rc_self.clone().borrow_mut().monkeys.insert(new_monkey_name.clone(), Rc::new(new_monkey));
-
-                    rc_self.clone().borrow_mut().monkeys.insert(
-                        root_name,
-                        Rc::new(Monkey::Math { 
-                            op: Op::from("="),
-                            left: right.try_math_left().unwrap(),
-                            right: new_monkey_name,
-                        }),
-                    );
+                    v_level_2 = Some(llv);
+                    v_level_2_is_left = true;
+                    t_level_2 = Some(left.try_math_right().unwrap());
+                    to_remove.push(left.try_math_left().unwrap().clone());
                 }
 
                 // Left right is the other constant
                 else if let Some(lrv) = rc_self.clone().borrow().monkeys[&left.try_math_right().unwrap()].try_constant_value() {
-                    if left.try_math_right().unwrap() == humn_name {
-                        break 'found_human;
-                    }
-
-                    // L = rv
-                // Lop(LL, lrv) = rv
-                // LR = Lop'(rv, lrv)
-                    let op_name = left.try_op_name().unwrap();
-                    let new_monkey_name = format!("{}{:?}{}", rv, op_name, lrv);
-                    let new_monkey = Monkey::Constant { value: (Op::from_inverse(op_name.as_str()).f)(rv, lrv) };
-
-                    rc_self.clone().borrow_mut().monkeys.insert(new_monkey_name.clone(), Rc::new(new_monkey));
-
-                    rc_self.clone().borrow_mut().monkeys.insert(
-                        root_name,
-                        Rc::new(Monkey::Math { 
-                            op: Op::from("="),
-                            left: right.try_math_left().unwrap(),
-                            right: new_monkey_name,
-                        }),
-                    );
+                    v_level_2 = Some(lrv);
+                    v_level_2_is_left = false;
+                    t_level_2 = Some(left.try_math_left().unwrap());
+                    to_remove.push(left.try_math_right().unwrap().clone());
                 }
 
                 // Something went wrong
@@ -356,24 +322,66 @@ impl Troop {
             else {
                 panic!("neither left nor root of root is constant, left: {left:?}, right: {right:?}");
             }
+                
+            // Build and attach the new root and frankenmonkey
+
+            // Calculate the various possible inverse functions
+            // The annoying one was [[v2 - SUB] = v1] since you need to subtract v2 and then negate
+            // I haven't handled all of the cases, just the ones that actually show up in the problem
+            let op_name = op_name.unwrap();
+            let f_inverse = match (op_name.as_str(), v_level_2_is_left) {
+                ("+", _)        => |v1, v2| v1 - v2,
+                ("-", true)     => |v1, v2| -1 * (v1 - v2),
+                ("-", false)    => |v1, v2| v1 + v2,
+                ("*", _)        => |v1, v2| v1 / v2,
+                ("/", false)    => |v1, v2| v1 * v2,
+
+                _ => panic!("unknown pattern ({op_name}, {v_level_2_is_left})"),
+            };
+
+            // Generate new, unique names for each monkey
+            let new_monkey_name = format!("C_{name_count}");
+            name_count += 1;
+
+            // Build and insert the new monkey with a constant value based on f_inverse above
+            let new_monkey = Monkey::Constant { value: f_inverse(v_level_1.unwrap(), v_level_2.unwrap()) };
+            rc_self.clone().borrow_mut().monkeys.insert(new_monkey_name.clone(), Rc::new(new_monkey));
+
+            // Figure out which side we should re-insert the new and old SUB monkes
+            let t_level_2_name = t_level_2.unwrap();
+            let left_name = if v_level_1_is_left { new_monkey_name.clone() } else { t_level_2_name.clone() };
+            let right_name = if v_level_1_is_left { t_level_2_name.clone() } else { new_monkey_name.clone() };
+
+            // Replace the new root node with one a single level down
+            rc_self.clone().borrow_mut().monkeys.insert(
+                String::from("root"),
+                Rc::new(Monkey::Math { 
+                    op: Op::from("="),
+                    left: left_name,
+                    right: right_name,
+                }),
+            );
             
+            // Remove the nodes that we no longer have in our tree (the constant values + what became the root)
+            for name in to_remove.into_iter() {
+                rc_self.borrow_mut().monkeys.remove(&name);
+            }
 
-            // DEBUG
-            println!("{root:?}, {left:?}, {right:?}");
-            break; 
-
+            if cfg!(debug_assertions) {
+                let mut f = File::create(format!("aoc16_{_i:04}.dot")).unwrap();
+                writeln!(&mut f, "{}\n", rc_self.borrow().dot(format!("g{_i:04}").as_str())).unwrap();
+            }
             
-
         }
     }
 
     #[allow(dead_code)]
-    fn dot(&self) -> String {
-        let mut result = String::from("graph G {\n");
+    fn dot(&self, graph_name: &str) -> String {
+        let mut result = format!("graph {graph_name} {{\n");
         for (name, monkey) in self.monkeys.iter() {
             match monkey.as_ref() {
                 Monkey::Constant { value } => {
-                    result.push_str(format!("\t{name} [label=\"{name}\\n{value}\"];\n").as_str());
+                    result.push_str(format!("\t\"{graph_name}.{name}\" [label=\"{name}\\n{value}\"];\n").as_str());
                 }
                 Monkey::Math {
                     op: Op {name: op_name, ..},
@@ -381,8 +389,12 @@ impl Troop {
                     right,
                     ..
                 } => {
-                    result.push_str(format!("\t{name} [label=\"{name}\\n{op_name}\"];").as_str());
-                    result.push_str(format!(" {name} -- {left}, {right};\n").as_str());
+                    result.push_str(format!("\t\"{graph_name}.{name}\" [label=\"{name}\\n{op_name}\", ordering=\"out\"];\n").as_str());
+                    result.push_str(format!("\t\t\"{graph_name}.{name}\" -- \"{graph_name}.{left}\" [label=L];\n").as_str());
+                    result.push_str(format!("\t\t\"{graph_name}.{name}\" -- \"{graph_name}.{right}\" [label=R];\n").as_str());
+                },
+                Monkey::Human => {
+                    result.push_str(format!("\t\"{graph_name}.humn\" [label=\"HELPME!\"];\n").as_str());
                 }
             }
         }
@@ -417,35 +429,33 @@ fn part2(filename: &Path) -> String {
         troop.add(&line);
     }
 
-    println!("before: {}", troop.monkeys.len());
-    troop.simplify_constants();
-    println!("{}", troop.dot());
-    println!("after simplify constant: {}", troop.monkeys.len());
-    troop.simplify_equality();
-    println!("after simplify equality: {}", troop.monkeys.len());
-    todo!();
-
-    let mut human_value = 0; // nihilism, yo
-    loop {
-        {
-            let me = troop.monkeys.get_mut(&String::from("humn")).unwrap();
-            match me.as_ref() {
-                Monkey::Constant { value } => {
-                    *value = human_value;
-                }
-                _ => panic!("haven't deal with non-constant me yet"),
-            }
-        }
-
-        let result = troop.value(&String::from("root"));
-        if result == 1 {
-            break;
-        }
-
-        human_value += 1; // better?
+    troop.monkeys.insert(String::from("humn"), Rc::new(Monkey::Human));
+    
+    if cfg!(debug_assertions) {
+        let mut f = File::create(format!("aoc16_0a_initial.dot")).unwrap();
+        writeln!(&mut f, "{}\n", troop.dot(format!("0a_initial").as_str())).unwrap();
     }
 
-    human_value.to_string()
+    troop.simplify_constants();
+    
+    if cfg!(debug_assertions) {
+        let mut f = File::create(format!("aoc16_0b_constants.dot")).unwrap();
+        writeln!(&mut f, "{}\n", troop.dot(format!("0b_constants").as_str())).unwrap();
+    }
+
+    troop.simplify_equality();
+
+    let root = &troop.monkeys[&String::from("root")];
+    let left = &troop.monkeys[&root.try_math_left().unwrap()];
+    let right = &troop.monkeys[&root.try_math_right().unwrap()];
+
+    if left.is_human() {
+        right.try_constant_value().unwrap().to_string()
+    } else if right.is_human() {
+        left.try_constant_value().unwrap().to_string()
+    } else {
+        panic!("absolutely murdered the human")
+    }
 }
 
 fn main() {
@@ -459,11 +469,11 @@ mod tests {
 
     #[test]
     fn test1() {
-        aoc_test("", part1, "")
+        aoc_test("21", part1, "31017034894002")
     }
 
     #[test]
     fn test2() {
-        aoc_test("", part2, "")
+        aoc_test("21", part2, "3555057453229")
     }
 }
