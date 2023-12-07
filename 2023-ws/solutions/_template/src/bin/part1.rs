ca9db2bb7b3bb70129ae1768298e932569a86cdf
JP Verkamp <jverkamp@dropbox.com>
2023-12-07 11:50:46 -0500

    Add just init with automatic fill

--- 2023-ws/solutions/_template/src/bin/part1.rs
+++ 2023-ws/solutions/_template/src/bin/part1.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use std::io::{self, BufRead};
 
-// #[aoc_test("data/test/00.txt", "")]
-// #[aoc_test("data/00.txt", "")]
+// #[aoc_test("data/test/__day__.txt", "")]
+// #[aoc_test("data/__day__.txt", "")]
 fn main() -> Result<()> {
     let stdin = io::stdin();
     let lines = stdin.lock().lines();
