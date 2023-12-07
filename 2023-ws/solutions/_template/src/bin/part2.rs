ff9f532742c725e058dbf0dc35e7bddaacfddef3
JP Verkamp <jverkamp@dropbox.com>
2023-12-07 11:56:46 -0500

    Import types in template

--- 2023-ws/solutions/_template/src/bin/part2.rs
+++ 2023-ws/solutions/_template/src/bin/part2.rs
@@ -1,13 +1,16 @@
 use anyhow::Result;
-use std::io::{self, BufRead};
+use std::{io::{self, BufRead}, fs::read_to_string};
+
+use day__day__::types::*;
 
 // #[aoc_test("data/test/__day__.txt", "")]
 // #[aoc_test("data/__day__.txt", "")]
 fn main() -> Result<()> {
     let stdin = io::stdin();
-    let lines = stdin.lock().lines();
+    // let lines = stdin.lock().lines();
+    // let input = io::read_to_string(stdin.lock())?;
 
-    let result = format!("TODO: got {} lines of input", lines.count());
+    let result = "TODO";
 
     println!("{result}");
     Ok(())
