3638b3a1fcee10010ef64b51ac44c0ca7edf83e0
JP Verkamp <jverkamp@dropbox.com>
2023-12-06 23:47:37 -0500

    Implement map apply for ranges in addition to values

--- 2023/src/bin/05-growinator.rs
+++ 2023/src/bin/05-growinator.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use aoc::*;
-use std::path::Path;
+use std::{ops::RangeInclusive, path::Path};
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Category {
@@ -14,6 +14,7 @@ pub enum Category {
     Location,
 }
 
+// A range mapping; defines a range from src..=(src+len) to dst..=(dst+len)
 #[derive(Debug)]
 pub struct RangeMap {
     src: u64,
@@ -22,12 +23,53 @@ pub struct RangeMap {
 }
 
 impl RangeMap {
+    // If x is in the source range, map to the destination
     pub fn apply(&self, x: u64) -> Option<u64> {
         if x < self.src || x >= self.src + self.len {
             None
         } else {
             Some(self.dst + x - self.src)
         }
+    }
+
+    // Apply over an input range
+    // Returns three optional ranges:
+    // 1. The portion of the original range below self's range
+    // 2. The portion of the original range overlapping self's range mapped to destination
+    // 3. The portion of the original range above self's range
+    #[allow(clippy::type_complexity)]
+    pub fn apply_range(
+        &self,
+        input: RangeInclusive<u64>,
+    ) -> (
+        Option<RangeInclusive<u64>>,
+        Option<RangeInclusive<u64>>,
+        Option<RangeInclusive<u64>>,
+    ) {
+        let (input_start, input_end) = input.clone().into_inner();
+        let src_end = self.src + self.len - 1;
+
+        let below = if input_start < self.src {
+            Some(input_start..=self.src.saturating_sub(1).min(input_end))
+        } else {
+            None
+        };
+
+        let overlap = if input_end >= self.src && input_start <= src_end {
+            let overlap_start = input_start.max(self.src);
+            let overlap_end = input_end.min(src_end);
+            Some((self.dst + overlap_start - self.src)..=(self.dst + overlap_end - self.src))
+        } else {
+            None
+        };
+
+        let above = if input_end > src_end {
+            Some(src_end.saturating_add(1).max(input_start)..=input_end)
+        } else {
+            None
+        };
+
+        (below, overlap, above)
     }
 }
 
@@ -44,6 +86,38 @@ impl CategoryMap {
             .iter()
             .find_map(|range_map| range_map.apply(x))
             .unwrap_or(x)
+    }
+
+    pub fn apply_range(&self, input: RangeInclusive<u64>) -> Vec<RangeInclusive<u64>> {
+        let mut ranges = vec![input.clone()];
+        let mut result = vec![];
+
+        for range_map in self.range_maps.iter() {
+            let mut unchanged = vec![];
+
+            // Mapped ranges are ready to return
+            // Anything else passes to the next range map
+            for range in ranges.iter() {
+                let (below, overlap, above) = range_map.apply_range(range.clone());
+                if let Some(below) = below {
+                    unchanged.push(below);
+                }
+                if let Some(overlap) = overlap {
+                    result.push(overlap);
+                }
+                if let Some(above) = above {
+                    unchanged.push(above);
+                }
+            }
+
+            ranges.clear();
+            ranges.append(&mut unchanged);
+        }
+
+        // Any unchanged ranges after all maps are returned
+        result.append(&mut ranges);
+
+        result
     }
 }
 
@@ -141,7 +215,8 @@ fn part1(filename: &Path) -> Result<String> {
     Ok(values.iter().min().unwrap().to_string())
 }
 
-fn part2(filename: &Path) -> Result<String> {
+#[allow(dead_code)]
+fn part2_brute(filename: &Path) -> Result<String> {
     let input = std::fs::read_to_string(filename)?;
     let (s, mut simulation) = parse::simulation(&input).unwrap();
     assert_eq!(s, "");
@@ -165,6 +240,41 @@ fn part2(filename: &Path) -> Result<String> {
     );
     assert_eq!(cat, Category::Location);
     Ok(values.iter().min().unwrap().to_string())
+}
+
+fn part2(filename: &Path) -> Result<String> {
+    let input = std::fs::read_to_string(filename)?;
+    let (s, simulation) = parse::simulation(&input).unwrap();
+    assert_eq!(s, "");
+
+    // Replace seeds with ranges
+    let ranges = simulation
+        .seeds
+        .chunks(2)
+        .map(|lo_hi| lo_hi[0]..=(lo_hi[0] + lo_hi[1]))
+        .collect::<Vec<_>>();
+
+    let (cat, values) =
+        simulation
+            .range_maps
+            .iter()
+            .fold((Category::Seed, ranges), |(cat, values), range_map| {
+                assert_eq!(cat, range_map.src_cat);
+                (
+                    range_map.dst_cat,
+                    values
+                        .iter()
+                        .flat_map(|r| range_map.apply_range(r.clone()))
+                        .collect(),
+                )
+            });
+    assert_eq!(cat, Category::Location);
+    Ok(values
+        .iter()
+        .map(|r| r.clone().min().unwrap())
+        .min()
+        .unwrap()
+        .to_string())
 }
 
 fn main() {
@@ -212,6 +322,34 @@ mod tests {
     }
 
     #[test]
+    fn test_range_map_range_apply() {
+        let range_map = super::RangeMap {
+            src: 5,
+            dst: 10,
+            len: 10,
+        };
+        // all low
+        assert_eq!(range_map.apply_range(1..=4), (Some(1..=4), None, None));
+        // low and mid
+        assert_eq!(
+            range_map.apply_range(1..=6),
+            (Some(1..=4), Some(10..=11), None)
+        );
+        // all mid
+        assert_eq!(range_map.apply_range(6..=14), (None, Some(11..=19), None));
+        // mid and high
+        assert_eq!(
+            range_map.apply_range(6..=16),
+            (None, Some(11..=19), Some(15..=16))
+        );
+        // all three
+        assert_eq!(
+            range_map.apply_range(1..=15),
+            (Some(1..=4), Some(10..=19), Some(15..=15))
+        );
+    }
+
+    #[test]
     fn test1() {
         aoc_test("test/05", part1, "35");
         aoc_test("05", part1, "825516882");
@@ -220,6 +358,6 @@ mod tests {
     #[test]
     fn test2() {
         aoc_test("test/05", part2, "46");
-        // aoc_test("05", part2, "136096660");
+        aoc_test("05", part2, "136096660");
     }
 }
