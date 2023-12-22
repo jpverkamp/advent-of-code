6d34e57ab9cb966d704c891c40dd08daf45f101b
JP Verkamp <jverkamp@dropbox.com>
2023-12-21 20:56:34 -0500

    Add From<Iterator> to bounds

--- 2023/libs/bounds/src/lib.rs
+++ 2023/libs/bounds/src/lib.rs
@@ -23,3 +23,29 @@ impl Bounds {
         self.max_y = self.max_y.max(p.y);
     }
 }
+
+impl<'a, I> From<I> for Bounds
+where
+    I: IntoIterator<Item = &'a Point>,
+{
+    fn from(value: I) -> Self {
+        let mut bounds = Bounds::default();
+        for p in value {
+            bounds.include(*p);
+        }
+        bounds
+    }
+}
+
+impl std::ops::Add<Bounds> for Bounds {
+    type Output = Bounds;
+
+    fn add(self, rhs: Bounds) -> Self::Output {
+        Bounds {
+            min_x: self.min_x.min(rhs.min_x),
+            max_x: self.max_x.max(rhs.max_x),
+            min_y: self.min_y.min(rhs.min_y),
+            max_y: self.max_y.max(rhs.max_y),
+        }
+    }
+}
