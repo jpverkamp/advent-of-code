920dee3f68e357bc01cf86f4ae987d56573e0e38
JP Verkamp <jverkamp@dropbox.com>
2024-12-03 01:02:32 -0500

    Add a second version using a state machine

--- 2024/src/day3.rs
+++ 2024/src/day3.rs
@@ -1,8 +1,8 @@
 use aoc_runner_derive::aoc;
 use regex::Regex;
 
-#[aoc(day3, part1)]
-fn part1(input: &str) -> u32 {
+#[aoc(day3, part1, regex)]
+fn part1_regex(input: &str) -> u32 {
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
     re.captures_iter(input)
@@ -10,8 +10,68 @@ fn part1(input: &str) -> u32 {
         .sum::<u32>()
 }
 
-#[aoc(day3, part2)]
-fn part2(input: &str) -> u32 {
+#[aoc(day3, part1, iterator)]
+fn part1(input: &str) -> u32 {
+    let input = input.chars().collect::<Vec<_>>();
+
+    #[derive(Debug)]
+    enum State {
+        Scanning,
+        ReadingA,
+        ReadingB,
+    }
+
+    let mut sum = 0;
+    let mut a = 0;
+    let mut b = 0;
+
+    let mut state = State::Scanning;
+    let mut index = 0;
+
+    loop {
+        match state {
+            State::Scanning => {
+                if input[index..].starts_with(&['m', 'u', 'l', '(']) {
+                    state = State::ReadingA;
+                    a = 0;
+                    b = 0;
+                    index += 3;
+                }
+            }
+            State::ReadingA => {
+                if input[index] == ',' {
+                    state = State::ReadingB;
+                } else if input[index].is_ascii_digit() {
+                    a = a * 10 + input[index] as u32 - '0' as u32;
+                } else {
+                    state = State::Scanning;
+                    index -= 1; // Recheck incase we start another mul
+                }
+            }
+            State::ReadingB => {
+                if input[index] == ')' {
+                    sum += a * b;
+                    state = State::Scanning;
+                } else if input[index].is_ascii_digit() {
+                    b = b * 10 + input[index] as u32 - '0' as u32;
+                } else {
+                    state = State::Scanning;
+                    index -= 1; // Recheck incase we start another mul
+                }
+            }
+        }
+
+        index += 1;
+        if index >= input.len() {
+            break;
+        }
+    }
+
+    sum
+}
+
+#[aoc(day3, part2, regex)]
+fn part2_regex(input: &str) -> u32 {
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
 
     re.captures_iter(input)
@@ -27,6 +87,76 @@ fn part2(input: &str) -> u32 {
         .0
 }
 
+#[aoc(day3, part2, iterator)]
+fn part2(input: &str) -> u32 {
+    let input = input.chars().collect::<Vec<_>>();
+
+    #[derive(Debug)]
+    enum State {
+        Scanning,
+        Disabled,
+        ReadingA,
+        ReadingB,
+    }
+
+    let mut sum = 0;
+    let mut a = 0;
+    let mut b = 0;
+
+    let mut state = State::Scanning;
+    let mut index = 0;
+
+    loop {
+        match state {
+            State::Scanning => {
+                if input[index..].starts_with(&['m', 'u', 'l', '(']) {
+                    state = State::ReadingA;
+                    a = 0;
+                    b = 0;
+                    index += 3;
+                } else if input[index..].starts_with(&['d', 'o', 'n', '\'', 't', '(', ')']) {
+                    state = State::Disabled;
+                    index += 6;
+                }
+            }
+            State::Disabled => {
+                if input[index..].starts_with(&['d', 'o', '(', ')']) {
+                    state = State::Scanning;
+                    index += 3;
+                }
+            }
+            State::ReadingA => {
+                if input[index] == ',' {
+                    state = State::ReadingB;
+                } else if input[index].is_ascii_digit() {
+                    a = a * 10 + input[index] as u32 - '0' as u32;
+                } else {
+                    state = State::Scanning;
+                    index -= 1; // Recheck incase we start another mul
+                }
+            }
+            State::ReadingB => {
+                if input[index] == ')' {
+                    sum += a * b;
+                    state = State::Scanning;
+                } else if input[index].is_ascii_digit() {
+                    b = b * 10 + input[index] as u32 - '0' as u32;
+                } else {
+                    state = State::Scanning;
+                    index -= 1; // Recheck incase we start another mul
+                }
+            }
+        }
+
+        index += 1;
+        if index >= input.len() {
+            break;
+        }
+    }
+
+    sum
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -35,6 +165,16 @@ mod tests {
     fn part1_example() {
         assert_eq!(
             part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
+            161
+        );
+    }
+
+    #[test]
+    fn part1_iterator_example() {
+        assert_eq!(
+            part1_iterator(
+                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+            ),
             161
         );
     }
