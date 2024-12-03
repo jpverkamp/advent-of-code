0df06f622855b0e2aa7985e2cb2df7ce92e3a0cc
JP Verkamp <jverkamp@dropbox.com>
2024-12-03 01:04:50 -0500

    Fix tests

--- 2024/src/day3.rs
+++ 2024/src/day3.rs
@@ -162,9 +162,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1_example() {
+    fn part1_regex_example() {
         assert_eq!(
-            part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
+            part1_regex("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
             161
         );
     }
@@ -172,10 +172,18 @@ mod tests {
     #[test]
     fn part1_iterator_example() {
         assert_eq!(
-            part1_iterator(
-                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-            ),
+            part1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
             161
+        );
+    }
+
+    #[test]
+    fn part2_regex_example() {
+        assert_eq!(
+            part2_regex(
+                "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+            ),
+            48
         );
     }
 
