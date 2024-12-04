4a0674d8292559240cb96a284cdac1a7ee55c74f
JP Verkamp <jverkamp@dropbox.com>
2024-12-04 13:05:00 -0500

    Fmt

--- 2024/src/day3.rs
+++ 2024/src/day3.rs
@@ -199,7 +199,9 @@ mod tests {
     #[test]
     fn part1_iterator_example() {
         assert_eq!(
-            part1_iterator("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
+            part1_iterator(
+                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+            ),
             161
         );
     }
@@ -217,7 +219,9 @@ mod tests {
     #[test]
     fn part2_example() {
         assert_eq!(
-            part2_iterator("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"),
+            part2_iterator(
+                "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+            ),
             48
         );
     }
