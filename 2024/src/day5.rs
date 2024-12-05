ca8d0b22dedef0f67e4bd1aa580642c3accc51b2
JP Verkamp <jverkamp@dropbox.com>
2024-12-05 15:43:17 -0500

    Implement as a vec of pairs, but sorted with binary search

--- 2024/src/day5.rs
+++ 2024/src/day5.rs
@@ -2,22 +2,24 @@ use aoc_runner_derive::{aoc, aoc_generator};
 
 #[derive(Debug, Clone)]
 pub struct Ordering {
-    data: [u32; 100*100]
+    data: Vec<(u32, u32)>,
 }
 
 impl Ordering {
     pub fn new() -> Self {
         Self {
-            data: [0; 100*100],
+            data: Vec::with_capacity(2000),
         }
     }
 
     pub fn insert(&mut self, a: u32, b: u32) {
-        self.data[(a as usize)*100+(b as usize)] = 1;
+        let point = (a, b);
+        let index = self.data.binary_search(&(a, b)).unwrap_or_else(|e| e);
+        self.data.insert(index, point);
     }
 
     pub fn can_precede(&self, a: u32, b: u32) -> bool {
-        !self.data[(a as usize)*100+(b as usize)].eq(&0)
+        self.data.binary_search(&(a, b)).is_ok()
     }
 
     pub fn validates(&self, list: &[u32]) -> bool {
