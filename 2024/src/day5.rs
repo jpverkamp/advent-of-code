6ffcd59911705a9a23d06262cd9b10f16cbc8445
JP Verkamp <jverkamp@dropbox.com>
2024-12-05 15:38:30 -0500

    Implement Ordering as a vector of the (a, b) pairs

--- 2024/src/day5.rs
+++ 2024/src/day5.rs
@@ -2,22 +2,22 @@ use aoc_runner_derive::{aoc, aoc_generator};
 
 #[derive(Debug, Clone)]
 pub struct Ordering {
-    data: [u32; 100*100]
+    data: Vec<(u32, u32)>,
 }
 
 impl Ordering {
     pub fn new() -> Self {
         Self {
-            data: [0; 100*100],
+            data: Vec::with_capacity(200),
         }
     }
 
     pub fn insert(&mut self, a: u32, b: u32) {
-        self.data[(a as usize)*100+(b as usize)] = 1;
+        self.data.push((a, b));
     }
 
     pub fn can_precede(&self, a: u32, b: u32) -> bool {
-        !self.data[(a as usize)*100+(b as usize)].eq(&0)
+        self.data.contains(&(a, b))
     }
 
     pub fn validates(&self, list: &[u32]) -> bool {
