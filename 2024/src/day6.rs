6b9246316fcb7aaf315a890692ef1eade97aba4d
JP Verkamp <jverkamp@dropbox.com>
2024-12-06 16:46:01 -0500

    Clean up code; add limited check

--- 2024/src/day6.rs
+++ 2024/src/day6.rs
@@ -1,5 +1,6 @@
 use crate::{Direction, Grid, Point};
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::iproduct;
 
 #[derive(Debug, Copy, Clone, Default)]
 enum Tile {
@@ -40,72 +41,78 @@ fn parse(input: &str) -> Map {
     }
 }
 
-#[aoc(day6, part1, v1)]
-fn part1_v1(input: &Map) -> usize {
-    let Map {
-        mut player,
-        mut facing,
-        grid,
-    } = input;
-    let mut path = Grid::new(grid.width, grid.height);
+impl Map {
+    fn visited(&self, check_loops: bool) -> Option<Grid<bool>> {
+        let Map {
+            mut player,
+            mut facing,
+            grid,
+        } = self;
 
-    path.set(player, true);
+        let mut visited = Grid::new(grid.width, grid.height);
+        visited.set(player, true);
 
-    while grid.in_bounds(player) {
-        match grid.get(player + facing) {
-            Some(Tile::Empty) => {
-                player += facing.into();
-                path.set(player, true);
+        let mut duplicates = hashbrown::HashSet::new();
+        duplicates.insert((player, facing));
+
+        while grid.in_bounds(player) {
+            match grid.get(player + facing) {
+                Some(Tile::Empty) => {
+                    player += facing.into();
+                    visited.set(player, true);
+                }
+                Some(Tile::Wall) => {
+                    facing = facing.rotate_cw();
+                }
+                None => break,
             }
-            Some(Tile::Wall) => {
-                facing = facing.rotate_cw();
+
+            if check_loops {
+                if duplicates.contains(&(player, facing)) {
+                    return None;
+                }
+                duplicates.insert((player, facing));
             }
-            None => break,
         }
-    }
 
-    path.iter().filter(|&&b| b).count()
+        Some(visited)
+    }
 }
 
+#[aoc(day6, part1, v1)]
+fn part1_v1(input: &Map) -> usize {
+    input.visited(false).unwrap().iter().filter(|&v| *v).count()
+}
+
+// For each point on the grid, check if adding a wall there would create a loop
 #[aoc(day6, part2, v1)]
 fn part2_v1(input: &Map) -> usize {
-    let mut looping = 0;
+    iproduct!(0..input.grid.width, 0..input.grid.height,)
+        .filter(|&(x, y)| {
+            let mut input = input.clone();
+            input.grid.set((x, y), Tile::Wall);
 
-    for x in 0..input.grid.width {
-        for y in 0..input.grid.height {
-            let Map {
-                mut player,
-                mut facing,
-                grid,
-            } = input;
-            let mut grid = grid.clone();
-            grid.set((x, y), Tile::Wall);
+            input.visited(true).is_none()
+        })
+        .count()
+}
 
-            let mut seen = hashbrown::HashSet::new();
-            seen.insert((player, facing));
-
-            while grid.in_bounds(player) {
-                match grid.get(player + facing) {
-                    Some(Tile::Empty) => {
-                        player += facing.into();
-                    }
-                    Some(Tile::Wall) => {
-                        facing = facing.rotate_cw();
-                    }
-                    None => break,
-                }
-
-                if seen.contains(&(player, facing)) {
-                    looping += 1;
-                    break;
-                }
-
-                seen.insert((player, facing));
-            }
-        }
-    }
-
-    looping
+// Only check walls on or adjacent to an originally visited path
+#[aoc(day6, part2, limited)]
+fn part2_limited(input: &Map) -> usize {
+    let visited = input.visited(false).unwrap();
+    iproduct!(0..input.grid.width, 0..input.grid.height)
+        .filter(|&(x, y)| {
+            let p = Point::from((x, y));
+            visited.get(p) == Some(&true)
+                || p.neighbors().iter().any(|&p| visited.get(p) == Some(&true))
+        })
+        .filter(|&(x, y)| {
+            let mut input = input.clone();
+            input.grid.set((x, y), Tile::Wall);
+            input.visited(true).is_none()
+        })
+        .count()
 }
 
 #[cfg(test)]
@@ -146,6 +153,19 @@ mod tests {
     fn part2_final() {
         assert_eq!(
             part2_v1(&parse(include_str!("../input/2024/day6.txt"))),
+            1939
+        );
+    }
+
+    #[test]
+    fn part2_limited_example() {
+        assert_eq!(part2_limited(&parse(EXAMPLE)), 6);
+    }
+
+    #[test]
+    fn part2_limited_final() {
+        assert_eq!(
+            part2_limited(&parse(include_str!("../input/2024/day6.txt"))),
             1939
         );
     }
