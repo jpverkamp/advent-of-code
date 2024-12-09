ce0fe4d708eee9be0ef5a8326a488bafb8ad6215
JP Verkamp <jverkamp@dropbox.com>
2024-12-08 21:23:45 -0500

    Day 8; probably only version

---
+++ 2024/src/day8.rs
@@ -0,0 +1,111 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+use crate::Grid;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+enum Tile {
+    #[default]
+    Empty,
+    Tower(char),
+}
+
+#[aoc_generator(day8)]
+fn parse(input: &str) -> Grid<Tile> {
+    Grid::read(input, &|c| match c {
+        '.' => Tile::Empty,
+        _ => Tile::Tower(c),
+    })
+}
+
+#[aoc(day8, part1, v1)]
+fn part1_v1(input: &Grid<Tile>) -> usize {
+    let mut towers = hashbrown::HashMap::new();
+
+    for (point, tile) in input.iter_enumerate() {
+        if let Tile::Tower(c) = tile {
+            towers.entry(c).or_insert_with(Vec::new).push(point);
+        }
+    }
+
+    let mut antinodes = Grid::new(input.width, input.height);
+
+    for (_, points) in towers.iter() {
+        for p1 in points {
+            for p2 in points {
+                if p1 != p2 {
+                    antinodes.set(*p2 + *p2 - *p1, true);
+                    antinodes.set(*p1 + *p1 - *p2, true);
+                }
+            }
+        }
+    }
+
+    antinodes.iter().filter(|&b| *b).count()
+}
+
+#[aoc(day8, part2, v1)]
+fn part2_v1(input: &Grid<Tile>) -> usize {
+    let mut towers = hashbrown::HashMap::new();
+
+    for (point, tile) in input.iter_enumerate() {
+        if let Tile::Tower(c) = tile {
+            towers.entry(c).or_insert_with(Vec::new).push(point);
+        }
+    }
+
+    let mut antinodes = Grid::new(input.width, input.height);
+
+    for (_, points) in towers.iter() {
+        for p1 in points {
+            for p2 in points {
+                if p1 != p2 {
+                    let delta = *p2 - *p1;
+
+                    let mut p = *p1 + delta;
+                    while input.in_bounds(p) {
+                        antinodes.set(p, true);
+                        p += delta;
+                    }
+
+                    let mut p = *p1 - delta;
+                    while input.in_bounds(p) {
+                        antinodes.set(p, true);
+                        p -= delta;
+                    }
+                }
+            }
+        }
+    }
+
+    antinodes.iter().filter(|&b| *b).count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::make_test;
+
+    const EXAMPLE: &str = "\
+..........
+..........
+..........
+....a.....
+........a.
+.....a....
+..........
+......A...
+..........
+..........";
+
+    make_test!([part1_v1] => "day8.txt", 4, 299);
+    make_test!([part2_v1] => "day8.txt", 8, 1032);
+}
+
+// For codspeed
+pub fn part1(input: &str) -> String {
+    part1_v1(&parse(input)).to_string()
+}
+
+pub fn part2(input: &str) -> String {
+    part2_v1(&parse(input)).to_string()
+}
--- 2024/src/lib.rs
+++ 2024/src/lib.rs
@@ -5,6 +5,7 @@ pub mod day4;
 pub mod day5;
 pub mod day6;
 pub mod day7;
+pub mod day8;
 
 mod grid;
 use grid::Grid;
