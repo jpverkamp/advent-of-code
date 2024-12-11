858d471a2ed07a6d35249b434859ea0e85eed071
JP Verkamp <jverkamp@dropbox.com>
2024-12-11 12:16:37 -0500

    Day 11 - data structure experiments

--- 2024/src/day11.rs
+++ 2024/src/day11.rs
@@ -1,5 +1,7 @@
-use aoc_runner_derive::{aoc, aoc_generator};
 use hashbrown::HashMap;
+use std::collections::BTreeMap;
+
+use aoc_runner_derive::{aoc, aoc_generator};
 
 #[aoc_generator(day11)]
 fn parse(input: &str) -> Vec<u64> {
@@ -71,10 +73,10 @@ fn part1_recursive(input: &[u64]) -> usize {
     blink_recur(input, 25)
 }
 
-#[aoc(day11, part2, recursive)]
-fn part2_recursive(input: &[u64]) -> usize {
-    blink_recur(input, 75)
-}
+// #[aoc(day11, part2, recursive)]
+// fn part2_recursive(input: &[u64]) -> usize {
+//     blink_recur(input, 75)
+// }
 
 // Add memoization
 
@@ -119,6 +121,98 @@ fn part2_recursive_memo(input: &[u64]) -> usize {
     blink_recur_memo(input, 75)
 }
 
+// Try an association list
+
+fn blink_recur_memo_assoc(input: &[u64], count: usize) -> usize {
+    fn recur(cache: &mut Vec<(u64, usize, usize)>, value: u64, depth: usize) -> usize {
+        if let Some(r) = cache.iter().find_map(|(v, d, r)| {
+            if *v == value && *d == depth {
+                Some(*r)
+            } else {
+                None
+            }
+        }) {
+            return r;
+        }
+
+        let result = if depth == 0 {
+            1
+        } else if value == 0 {
+            recur(cache, 1, depth - 1)
+        } else {
+            let digit_count = value.ilog10() + 1;
+            if digit_count % 2 == 0 {
+                let divisor = 10u64.pow(digit_count / 2);
+                recur(cache, value / divisor, depth - 1) + recur(cache, value % divisor, depth - 1)
+            } else {
+                recur(cache, value * 2024, depth - 1)
+            }
+        };
+
+        cache.push((value, depth, result));
+        result
+    }
+
+    let mut cache = vec![];
+    input
+        .iter()
+        .map(|&v| recur(&mut cache, v, count))
+        .sum::<usize>()
+}
+
+#[aoc(day11, part1, recursive_memo_assoc)]
+fn part1_recursive_memo_assoc(input: &[u64]) -> usize {
+    blink_recur_memo_assoc(input, 25)
+}
+
+#[aoc(day11, part2, recursive_memo_assoc)]
+fn part2_recursive_memo_assoc(input: &[u64]) -> usize {
+    blink_recur_memo_assoc(input, 75)
+}
+
+// Try a btree
+
+fn blink_recur_memo_btree(input: &[u64], count: usize) -> usize {
+    fn recur(cache: &mut BTreeMap<(u64, usize), usize>, value: u64, depth: usize) -> usize {
+        if let Some(&v) = cache.get(&(value, depth)) {
+            return v;
+        }
+
+        let result = if depth == 0 {
+            1
+        } else if value == 0 {
+            recur(cache, 1, depth - 1)
+        } else {
+            let digit_count = value.ilog10() + 1;
+            if digit_count % 2 == 0 {
+                let divisor = 10u64.pow(digit_count / 2);
+                recur(cache, value / divisor, depth - 1) + recur(cache, value % divisor, depth - 1)
+            } else {
+                recur(cache, value * 2024, depth - 1)
+            }
+        };
+
+        cache.insert((value, depth), result);
+        result
+    }
+
+    let mut cache = BTreeMap::new();
+    input
+        .iter()
+        .map(|&v| recur(&mut cache, v, count))
+        .sum::<usize>()
+}
+
+#[aoc(day11, part1, recursive_memo_btree)]
+fn part1_recursive_memo_btree(input: &[u64]) -> usize {
+    blink_recur_memo_btree(input, 25)
+}
+
+#[aoc(day11, part2, recursive_memo_btree)]
+fn part2_recursive_memo_btree(input: &[u64]) -> usize {
+    blink_recur_memo_btree(input, 75)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -126,7 +220,7 @@ mod tests {
 
     const EXAMPLE: &str = "125 17";
 
-    make_test!([part1_v1, part1_recursive, part1_recursive_memo] => "day11.txt", 55312, 194482);
+    make_test!([part1_v1, part1_recursive, part1_recursive_memo, part1_recursive_memo_assoc, part1_recursive_memo_btree] => "day11.txt", 55312, 194482);
     make_test!([part2_recursive_memo] => "day11.txt", "65601038650482", "232454623677743");
 }
 
