a28d04e16972953b87c9d7dfb493607afa8b3969
JP Verkamp <me@jverkamp.com>
2025-12-05 14:55:44 -0500

    Just because

--- 2025/src/bin/day5.rs
+++ 2025/src/bin/day5.rs
@@ -89,6 +89,32 @@ fn part2(input: &str) -> impl Into<String> {
         .to_string()
 }
 
+#[allow(dead_code)]
+// #[aoc::register]
+fn part2_bruteforce(input: &str) -> impl Into<String> {
+    let puzzle = Puzzle::from(input);
+
+    let min = puzzle.ranges.iter().map(|(a, _)| *a).min().unwrap();
+    let max = puzzle.ranges.iter().map(|(_, b)| *b).max().unwrap();
+
+    let start_time = std::time::Instant::now();
+
+    (min..=max)
+        .map(|id| {
+            if id % 100_000_000 == 0 {
+                let elapsed = start_time.elapsed().as_secs_f64();
+                let rate = (id - min) as f64 / elapsed;
+                let eta = (max  - id) as f64 / rate;
+                
+                println!("[{id}] Elapsed: {:.2} s, Rate: {:.2} ids/s, ETA: {:.2} s", elapsed, rate, eta);
+            }
+            id
+        })
+        .filter(|id| !puzzle.ranges.iter().any(|(a, b)| id >= a && id <= b))
+        .count()
+        .to_string()
+}
+
 aoc::test!(
     text = "\
 3-5
